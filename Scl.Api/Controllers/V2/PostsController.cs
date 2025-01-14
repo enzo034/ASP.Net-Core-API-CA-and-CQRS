488639dc8bea5149bdd44131192fb254650a1858
enzo034 <enzo03453@gmail.com>
2025-01-13 21:44:30 -0300

    Add project files.

---
+++ Scl.Api/Controllers/V2/PostsController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scl.Api.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id, Text = "Hello, universe" };
+            return Ok(post);
+        }
+    }
+}
